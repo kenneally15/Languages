47b9701e44e6c51eaab30f66355756ae2e292e19
kenneally15 <kenneally15@yahoo.com>
2025-03-07 15:52:40 -0500

    Java file compilation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,7 @@
-// HelloWorld.java
-
-// This is a simple Java program that prints "Hello, World" to the console.
 
 
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-      System.out.println("additional text");
-      System.out.println("even more text");
+      System.out.println("Hello, World!");
    }
 }
