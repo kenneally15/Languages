99f4e9cba074bfae1f41432ff7c78d0d46578fd8
kenneally15 <kenneally15@yahoo.com>
2025-03-06 21:52:57 -0500

    playing around with haskell functions

--- test.hs
+++ test.hs
@@ -1,7 +1,17 @@
-main = do putStrLn "What is 2 + 2?"
+--Haskell test file
+
+module Main where
+
+factorial n = if n == 0 then 1 else n * factorial (n-1)
+
+main = do putStrLn "What is 5! ?"
           x <- readLn
-          if x == 4
+          if x == factorial 5
               then putStrLn "You're right!"
               else putStrLn "You're wrong!"
+
+
+
+
 
 
